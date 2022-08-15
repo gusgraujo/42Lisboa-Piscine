54a55b86fe9d521f7e6be98fc28cac9060fd8c48
gusgraujo <gusg.raujo@gmail.com>
2022-08-15 21:38:11 +0100

    C04

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: gugoncal <gugoncal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 18:50:34 by gugoncal          #+#    #+#             */
-/*   Updated: 2022/08/15 19:49:31 by gugoncal         ###   ########.fr       */
+/*   Updated: 2022/08/15 21:35:20 by gugoncal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,23 @@ int	ft_atoi(char *str)
 {
 	int	i;
 	int	sign;
+	int	number;
 
 	i = 0;
+	number = 0;
+	sign = 1;
 	while (str[i] != '\0')
 	{
-
+		if (str[i] == '-')
+		{
+			sign = sign * -1;
+		}
+		if (str[i] < '0' && str[i] > '9')
+		{
+			return (0);
+		}
+		number = (number * 10) + (str[i] - '0');
+		i++;
 	}
+	return (sign * number);
 }
