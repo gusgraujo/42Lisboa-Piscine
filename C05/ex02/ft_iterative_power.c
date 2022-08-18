32bf6ba8cfb3d44e105f8466187f7de4c228e88a
gusgraujo <gusg.raujo@gmail.com>
2022-08-18 13:25:22 +0100

    C05/2

---
+++ C05/ex02/ft_iterative_power.c
@@ -0,0 +1,19 @@
+
+
+
+int ft_iterative_power(int nb, int power)
+{
+    int value;
+
+	value = nb;
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	while (power > 1)
+	{
+		value *= nb;
+		power--;
+	}
+	return (value);
+}
